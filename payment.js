08548c73f2696737de6d68c1064827e7986bb7ff
asagba90 <victorasagba90@gmail.com>
2022-10-30 09:31:18 +0100

    added esle to payment

---
+++ payment.js
@@ -0,0 +1,7 @@
+let newPayment = 9000;
+let oldPayment = 5000;
+if (newPayment > oldPayment) {
+    console.log (`you have balance in your account of ${newPayment - oldPayment}`)
+} else {
+    console.log("you have no payment");
+}
